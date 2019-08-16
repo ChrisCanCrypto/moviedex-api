2842925be6ea9bd4c09aba816fde86bb543e8197
Chris Wysocki <christopher.wysocki@gmail.com>
2019-08-15 19:40:24 -0700

    submitting for review

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIEDEX = require('./moviedex.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		return res.status(401).json({ error: 'Unauthorized request' });
+	}
+	// move to the next middleware
+	next();
+});
+
+function handleGetMovies(req, res) {
+	let results = MOVIEDEX;
+
+	const { genre, country, avg_vote } = req.query;
+
+	if (genre) {
+		results = results.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+	}
+
+	if (country) {
+		var regex = /%20/gi;
+		var countryField = country.replace(regex, ' ');
+		results = results.filter(movie =>
+			movie.country.toLowerCase().includes(countryField.toLowerCase())
+		);
+	}
+
+	if (avg_vote) {
+		results = results.filter(movie => movie.avg_vote >= parseFloat(avg_vote));
+	}
+
+	res.json(results);
+}
+
+app.get('/movie', handleGetMovies);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+	console.log(`Server listening at http://localhost:${PORT}`);
+});
